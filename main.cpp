1547f3fbce7afd4d48df9eff91730a5eb3af490b
Nasser alyou Mohammadou abdoul <namohamm@lab3r6s12.42abudhabi.ae>
2022-07-12 18:19:21 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namohamm <namohamm@student.42.ae>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 03:49:01 by namohamm          #+#    #+#             */
+/*   Updated: 2022/07/12 15:44:45 by namohamm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+using namespace std;
+
+class Personage
+{
+	int m_vie;
+	int m_mana;
+	string m_nomArme;
+	int m_degatsArme;
+};
+
+int main(void)
+{
+	cout << "Hello Nass" << endl;
+	return (0);
+}
