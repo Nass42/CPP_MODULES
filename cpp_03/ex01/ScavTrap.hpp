1683c9c97a80c382869a50a3229cf526c1e7134a
Nass42 <96660360+Nass42@users.noreply.github.com>
2022-12-03 14:58:01 +0000

    cpp_04 init

--- cpp_03/ex01/ScavTrap.hpp
+++ cpp_03/ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: namohamm <namohamm@student.42.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 00:28:27 by namohamm          #+#    #+#             */
-/*   Updated: 2022/12/02 00:40:37 by namohamm         ###   ########.fr       */
+/*   Updated: 2022/12/03 13:24:44 by namohamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,33 @@
 // its constructor and destructor and attack will print a
 // different message than the ones in ClapTrap
 
+/*
+** What is inheritance?
+** Inheritance is a way to reuse code. It allows you to create a new class
+** that reuses (inherits) all the properties and behaviors of an existing class.
+** The class whose properties and behaviors are inherited is called the base class.
+** The class that inherits the properties and behaviors of the base class is called
+** the derived class.
+** the derived class can add its own properties and behaviors in addition to the
+** inherited ones.
+** The derived class can also override the behavior of the base class.
+** Here the syntax:
+** class DerivedClassName : access BaseClassName {
+** 	// body of the derived class
+** }
+** access can be public, protected or private
+** access specifies the access level for the base class members in the derived class
+*/
+
+class ScavTrap : public ClapTrap {
+	public:
+		ScavTrap();
+		ScavTrap( std::string name );
+		ScavTrap( ScavTrap const & rhs );
+		ScavTrap & operator=( ScavTrap const & rhs );
+		~ScavTrap();
+		void attack( std::string const & target );
+		void guardGate();
+};
 
 #endif
--- cpp_03/ex01/main.cpp
+++ cpp_03/ex01/main.cpp
@@ -6,18 +6,17 @@
 /*   By: namohamm <namohamm@student.42.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 21:28:49 by namohamm          #+#    #+#             */
-/*   Updated: 2022/12/01 23:44:10 by namohamm         ###   ########.fr       */
+/*   Updated: 2022/12/03 13:04:23 by namohamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 int main(void) {
-	ClapTrap clapTrap("Nass");
-
-	clapTrap.attack("freeman");
-	clapTrap.takeDamage(9);
-	clapTrap.beRepaired(5);
+	ClapTrap clapTrap("ClapTrap");
+	ScavTrap scavTrap("ScavTrap");
+	clapTrap.attack("ClapTrap");
+	scavTrap.attack("ScavTrap");
 
 	return 0;
 }
