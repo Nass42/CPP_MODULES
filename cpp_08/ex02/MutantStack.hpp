e9e615f13a34eebaf936abaeba4ef5e8e06540c5
Nass42 <nassdoul3@gmail.com>
2022-12-24 12:10:11 +0400

    update

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -5,28 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: namohamm <namohamm@student.42.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/20 00:19:53 by namohamm          #+#    #+#             */
-/*   Updated: 2022/12/20 01:10:08 by namohamm         ###   ########.fr       */
+/*   Created: 2022/12/24 02:30:26 by namohamm          #+#    #+#             */
+/*   Updated: 2022/12/24 11:52:42 by namohamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-# include <iostream>
-# include <stack>
+#include <iostream>
+#include <stack>
 
 template <typename T>
-class MutantStack : public std::stack<T> {
+class MutantStack : public std::stack<T>
+{
 	public:
 		MutantStack() : std::stack<T>() {}
 		MutantStack(MutantStack const &src) : std::stack<T>(src) {}
 		~MutantStack() {}
-		MutantStack &operator=(MutantStack const &rhs) { std::stack<T>::operator=(rhs); return *this; }
-
+		MutantStack &operator=(MutantStack const &rhs)
+		{
+			if (this != &rhs)
+				std::stack<T>::operator=(rhs);
+			return (*this);
+		}
 		typedef typename std::stack<T>::container_type::iterator iterator;
-		iterator begin() { return std::stack<T>::c.begin(); }
-		iterator end() { return std::stack<T>::c.end(); }
+		iterator begin() { return (std::stack<T>::c.begin()); }
+		iterator end() { return (std::stack<T>::c.end()); }
 };
-
 #endif
---
+++ cpp_08/ex02/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namohamm <namohamm@student.42.ae>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/24 11:48:44 by namohamm          #+#    #+#             */
+/*   Updated: 2022/12/24 11:54:26 by namohamm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	std::cout << "------------------" << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	std::cout << "------------------" << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	std::cout << "------------------" << std::endl;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::cout << "------------------" << std::endl;
+	std::stack<int> s(mstack);
+	return 0;
+}
